c36a0a85b31f2392ab1d64d91ebcad97e1d23e6b
DravenWilson1 <dravenwilson@icloud.com>
2026-03-25 11:38:02 -0400

    Test environment setup and scripting for TC-U07

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    css: false,
+  },
   server: {
     proxy: {
       
